094ae9afd9c9fa3becd2ae893d7be04ecead393e
Viktoriia Veremiichuk <vikaveremiychuk@gmail.com>
2026-05-03 23:38:53 +0300

    hw1 express

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Simulated server error');
+});
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const id_param = req.params.noteId;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${id_param}`,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on localhost:3000');
+});
